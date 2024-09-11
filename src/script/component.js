1e49b4827fad35a2f5f5ce1c3dce46a3bf7513c0
mKangur <kangur.marcus@gmail.com>
2024-09-12 01:37:46 +0300

    header progress

--- src/script/component.js
+++ src/script/component.js
@@ -1,10 +1,48 @@
-export const Button = (props) => {
+export const UserInfo = (props) => {
     return (
-        <button
-            className="button"
-            onClick={props.onClick}
-        >
-            {props.children}
-        </button>
-    );
-}
+        `
+        <div class="user_info">
+            <span class="welcome_message">Welcome,
+                <span class="welcome_name">${props.firstName} ${props.lastName}</span>
+            </span>
+            <div id="logout">Logout</div>
+        </div>
+        `
+    )
+};
+
+export const LoginContainer = () => {
+    return (
+        `
+        <div id="login_container">
+            <form id="login_contents">
+                <h1 class="title">Graphql 📈📉</h1>
+                <h2 class="title_guide">Sign in to your intra account</h2>
+                <input type="text" name="username" id="username" placeholder="Username/Email" required>
+                <input type="password" name="password" id="password" placeholder="Password" required>
+                <input type="submit" value="Login" id="submit">
+            </div>
+        </div>
+        `
+    )
+};
+
+export const TabButton = (props) => {
+    return (
+        `
+        <div class="tab_button">
+            <button>${props.name}</button>
+        </div>
+        `
+    )
+};
+
+export const Header = () => {
+    return (
+        `
+        <header id="header">
+            <h1 class="title">Graphql 📈📉</h1>
+        </header>
+        `
+    )
+};
--- src/script/render.js
+++ src/script/render.js
@@ -1,30 +1,27 @@
 import * as query from './query.js';
+import * as e from './component.js';
 import * as util from './util.js';
 
 export async function Homepage() {
     const JWT_token = util.getTokenFromStorage();
-    const userInfo =  await util.fetchData(query.userQuery, JWT_token);
+    const user =  await util.fetchData(query.userQuery, JWT_token);
 
     let firstName;
     let lastName;
-    console.log("Userinfo: ", userInfo)
-    if (userInfo.error === undefined) {
-        firstName = userInfo.data.user[0].firstName;
-        lastName = userInfo.data.user[0].lastName;
+    console.log("user: ", user)
+    if (user.error === undefined) {
+        firstName = user.data.user[0].firstName;
+        lastName = user.data.user[0].lastName;
     }
 
-    console.log("Userinfo: ", userInfo)
+    console.log("user: ", user)
+    let header = e.Header();
+    let template = e.UserInfo({firstName, lastName});
 
-    let template =
-    `
-    <div class="user_info">
-        <span class="welcome_message">Welcome,
-            <span class="welcome_name">${firstName} ${lastName}</span>
-        </span>
-        <div id="logout">Logout</div>
-    </div>
-    `
-    document.getElementById("root").insertAdjacentHTML("afterbegin", template);
+    document.getElementById("root").insertAdjacentHTML("afterbegin", header);
+    document.getElementById("header").insertAdjacentHTML("beforeend", template);
+
+    // document.getElementById("root").insertAdjacentHTML("afterbegin", template);
 
     document.getElementById("logout").addEventListener("click", function () {
         util.logout();
@@ -33,18 +30,8 @@ export async function Homepage() {
 }
 
 export async function Loginpage() {
-    let template =
-    `
-    <div id="login_container">
-        <form id="login_contents">
-            <h1 class="title">Graphql 📈📉</h1>
-            <h2 class="title_guide">Sign in to your intra account</h2>
-            <input type="text" name="username" id="username" placeholder="Username/Email" required>
-            <input type="password" name="password" id="password" placeholder="Password" required>
-            <input type="submit" value="Login" id="submit">
-        </div>
-    </div>
-    `
+    let template = e.LoginContainer();
+
     document.getElementById("root").insertAdjacentHTML("afterbegin", template);
 
     // Event listener for login submit button
